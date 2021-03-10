c645b14c107e9c0d3822ba392f0c5e027680fbd0
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-03-10 05:21:23 -1200

    ciclo de vida de componetes

--- analizando-use-state/src/App.js
+++ analizando-use-state/src/App.js
@@ -1,20 +1,56 @@
-import { useState } from "react";
-function App() {
-  const [name, setName] = useState("Miguel");
-  const [age] = useState();
-  //const [, setSomething] = useState("Hey1");
+import { useState, Component } from "react";
 
-  const handleHeyClick = () => {
-    setName("Angel");
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+    console.log("Contructor");
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentDidUpdate(prevProps, prevState) {
+    //console.log(prevProps, prevState);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps, nextState);
+    return true;
+  }
 
-  return (
-    <div className="App">
-      <h2> Hola {name}</h2>
-      <p>{age}</p>
-      <button onClick={handleHeyClick}>Hey</button>
-    </div>
-  );
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.name}
+          onChange={({ target: { value } }) => this.setState({ name: value })}
+        ></input>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   const [name, setName] = useState("Miguel");
+//   const [age] = useState();
+//   //const [, setSomething] = useState("Hey1");
+
+//   const handleHeyClick = () => {
+//     setName("Angel");
+//   };
+
+//   return (
+//     <div className="App">
+//       <h2> Hola {name}</h2>
+//       <p>{age}</p>
+//       <button onClick={handleHeyClick}>Hey</button>
+//     </div>
+//   );
+// }
 
 export default App;
