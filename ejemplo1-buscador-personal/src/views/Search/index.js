48c56e0827f091ca106055aa6c5883eee448f9e4
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-03-19 08:26:56 -0600

    Usando estilos condicionales y propiedades (props)

--- ejemplo1-buscador-personal/src/views/Search/index.js
+++ ejemplo1-buscador-personal/src/views/Search/index.js
@@ -1,9 +1,18 @@
 import SearchBox from "./components/SearchBox";
+import { useState } from "react";
 import "./style.css";
 export default function Search() {
+  const [isAtTop, setIsAtTop] = useState(false);
+  const handleClaseOpenSearch = () => {
+    setIsAtTop(!isAtTop);
+  };
+
   return (
-    <div className="search">
-      <SearchBox></SearchBox>
+    <div className={`search ${isAtTop ? "search--top" : "search--center"} `}>
+      <SearchBox
+        onSearch={handleClaseOpenSearch}
+        onClose={handleClaseOpenSearch}
+      ></SearchBox>
     </div>
   );
 }
