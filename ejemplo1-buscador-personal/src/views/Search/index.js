c4410a42d84d2a86557331f0dd76a7d112e717b6
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-03-24 12:41:37 -0600

    Realizando llamadas a una API utilizando Fetch

--- ejemplo1-buscador-personal/src/views/Search/index.js
+++ ejemplo1-buscador-personal/src/views/Search/index.js
@@ -1,13 +1,24 @@
 import SearchBox from "./components/SearchBox";
-import { useState } from "react";
-import data from "../../data/users.json";
+import { useState, useEffect } from "react";
 import "./style.css";
 import SearchResults from "./components/SearchResults";
 
 export default function Search() {
   const [isAtTop, setIsAtTop] = useState(false);
-
   const [results, setResults] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setData(data);
+    };
+    getUsers().catch(null);
+  }, []);
+
   const handleCloseSearch = () => {
     setIsAtTop(false);
     setResults([]);
