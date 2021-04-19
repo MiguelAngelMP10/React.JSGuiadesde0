20923b38582c9c1ed68beb65741c2499e2ab622e
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-04-19 14:57:22 -0500

    Implementando ScrollToTop de React Router

--- introduccion-react-router/src/routes/index.js
+++ introduccion-react-router/src/routes/index.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FourOFour from "../views/404";
 import Home from "../views/Home";
 import PokeDatail from "../views/PokeDatail";
+import ScrollToTop from "../componets/ScrollToTop";
 
 export default function Routes() {
   return (
     <Router>
+      <ScrollToTop></ScrollToTop>
       <Switch>
         <Route path="/" exact>
           <Home></Home>
