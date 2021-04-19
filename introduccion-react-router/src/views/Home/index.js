4ea784236453e450754ca93cc71e9c9d58c40c81
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-04-19 14:57:22 -0500

    Implementando ScrollToTop de React Router

--- introduccion-react-router/src/views/Home/index.js
+++ introduccion-react-router/src/views/Home/index.js
@@ -3,10 +3,14 @@ import { useContext, useEffect } from "react";
 import PokemonContext from "../../context/pokemons";
 import PokemonList from "./components/PokemonList";
 export default function Home() {
-  const { getPokemons, pokemons } = useContext(PokemonContext);
+  const { getPokemons, pokemons, isLoading } = useContext(PokemonContext);
   useEffect(() => {
     getPokemons().catch(null);
   }, []);
+
+  if (isLoading) {
+    return <p>Cargando resultados....</p>;
+  }
 
   return (
     <div>
